6f1a1faa34cd78f33aca9b477a90db085c35016f
Minepapa <72640443+Minepapa@users.noreply.github.com>
2026-04-22 08:52:19 +0900

    Create Record.kt

---
+++ app/src/main/java/com/minepapa/kidsmoneyapp/Record.kt
@@ -0,0 +1,18 @@
+package com.minepapa.kidsmoneyapp
+
+data class Record(
+    val id: Long = 0,
+    val date: String,
+    val type: String,  // income, expense, tobank, frombank, direct_in
+    val memo: String,
+    val amount: Int
+) {
+    val isIncome: Boolean
+        get() = type == "income" || type == "frombank"
+
+    val isExpense: Boolean
+        get() = type == "expense"
+
+    val isBank: Boolean
+        get() = type == "tobank" || type == "direct_in"
+}
